41c0103da9a56f669ad842d769bf8d3a4df078a4
francesca sandford <fran6san@gmail.com>
2023-09-15 13:28:35 +0100

    feat: set up store command and add warehouse checks

---
+++ app/commands/store.rb
@@ -0,0 +1,19 @@
+require 'commands/command'
+require 'crate'
+
+class Store < Command
+  COMMAND = 'store'
+  ARGS = 'X Y W H P'
+  HELP = 'Stores a crate of product code P and of size W x H at position (X,Y). The crate will occupy W x H locations on the grid.'
+
+  def execute(args)
+    position_x, position_y, width, height = args.map(&:to_i)
+    product_code = args.last
+    crate = Crate.new(position_x, position_y, width, height, product_code)
+    if warehouse.store?(crate)
+      "> Crate (#{crate.width} x #{crate.height}) successfully stored at (#{crate.position_x},#{crate.position_y})."
+    else
+      "> Crate cannot be stored."
+    end
+  end
+end
--- app/warehouse.rb
+++ app/warehouse.rb
@@ -12,4 +12,43 @@ class Warehouse
     @height = height
     @grid = Array.new(height) { Array.new(width, " .") }
   end
+
+  def store?(crate)
+    start_y = crate.position_y - 1
+    start_x = crate.position_x - 1
+    end_y = crate.position_y + crate.height - 2
+    end_x = crate.position_x + crate.width - 2
+
+    if crate_can_be_placed?(crate, start_y, start_x, end_y, end_x)
+      (start_y..end_y).each do |row|
+        (start_x..end_x).each do |col|
+          @grid[row][col] = " #{crate.product_code}"
+        end
+      end
+      true
+    else
+      false
+    end
+  end
+
+  private
+
+  def crate_can_be_placed?(crate, start_y, start_x, end_y, end_x)
+    (start_y..end_y).each do |row|
+      (start_x..end_x).each do |col|
+        return false if @grid[row].nil? || @grid[col].nil?
+        return false if @grid.flatten.include?(" #{crate.product_code}") || @grid[row][col] != " ."
+      end
+    end
+
+    if crate.width > @width || crate.height > @height
+      false
+    elsif start_x > @width || end_x > @width
+      false
+    elsif start_y > @height || end_y > @height
+      false
+    else
+      true
+    end
+  end
 end
