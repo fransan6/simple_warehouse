060c41dd5a31faa3b0b1234534c828ca94ee3bfe
francesca sandford <fran6san@gmail.com>
2023-09-15 13:13:17 +0100

    feat: set up init command and testing

--- app/commands/init.rb
+++ app/commands/init.rb
@@ -6,6 +6,19 @@ class Init < Command
   HELP = 'Initialises the application as an empty W x H warehouse.'
 
   def execute(args)
-    "Not implemented"
+    if warehouse.width
+      puts "> IMPORTANT. This will fully reset your current warehouse (#{warehouse.width} x #{warehouse.height}) and remove any crate(s). Please confirm [y/n] if you would like to continue."
+      gets.chomp == 'y' ? create_warehouse(args) : "> No warehouse created - current warehouse retained (#{warehouse.width} x #{warehouse.height}). #{CommandLineInterface::HINT}"
+    else
+      create_warehouse(args)
+    end
+  end
+
+  private
+
+  def create_warehouse(args)
+    width, height = args.map(&:to_i)
+    warehouse.init(width, height)
+    "> Warehouse created (#{width} x #{height})."
   end
 end
--- spec/commands/init_spec.rb
+++ spec/commands/init_spec.rb
@@ -2,8 +2,46 @@ require 'spec_helper'
 require 'commands/init'
 
 describe Init do
+  let(:unit) { Warehouse.new }
+  let(:init) { described_class.new(unit) }
+
+  def created_warehouse(width, height)
+    expect(unit.grid).not_to be_empty
+    expect(unit.grid.count).to eq(height)
+    expect(unit.grid[0].count).to eq(width)
+  end
+
   describe '.command' do
     subject { described_class.command }
     it { is_expected.to eq 'init' }
+  end
+
+  describe '#execute' do
+    context 'when a user has not yet created their warehouse' do
+      it 'a warehouse is created with the provided dimensions' do
+        result = init.execute(['4', '3'])
+        created_warehouse(4, 3)
+        expect(result).to eq "> Warehouse created (4 x 3)."
+      end
+    end
+  end
+
+  context 'when a warehouse has already been created by the user' do
+    before(:each) do
+      init.execute(['5', '4'])
+    end
+
+    it 'a new warehouse is created with new dimensions if the user confirms `y`' do
+      allow(init).to receive(:gets).and_return('y')
+      result = init.execute(['6', '7'])
+      created_warehouse(6, 7)
+      expect(result).to eq "> Warehouse created (6 x 7)."
+    end
+
+    it 'no new warehouse is created if the user confirms `n`' do
+      allow(init).to receive(:gets).and_return('n')
+      result = init.execute(['6', '7'])
+      expect(result).to eq "> No warehouse created - current warehouse retained (5 x 4). Type `help` for instructions on usage."
+    end
   end
 end
