d12aa7ba4266d80b79a862c165206f2e4419b4b3
francesca sandford <fran6san@gmail.com>
2023-09-17 21:57:54 +0100

    refactor(warehouse): split logic into methods

--- app/warehouse.rb
+++ app/warehouse.rb
@@ -14,17 +14,15 @@ class Warehouse
   end
 
   def store?(crate)
-    start_y = crate.position_y - 1
-    start_x = crate.position_x - 1
-    end_y = crate.position_y + crate.height - 2
-    end_x = crate.position_x + crate.width - 2
+    positions = {
+      start_y: crate.position_y - 1,
+      start_x: crate.position_x - 1,
+      end_y: crate.position_y + crate.height - 2,
+      end_x: crate.position_x + crate.width - 2
+    }
 
-    if crate_can_be_placed?(crate, start_y, start_x, end_y, end_x)
-      (start_y..end_y).each do |row|
-        (start_x..end_x).each do |col|
-          @grid[row][col] = " #{crate.product_code}"
-        end
-      end
+    if crate_can_be_placed?(crate, positions)
+      place_crate(crate, positions)
       true
     else
       false
@@ -33,22 +31,34 @@ class Warehouse
 
   private
 
-  def crate_can_be_placed?(crate, start_y, start_x, end_y, end_x)
-    (start_y..end_y).each do |row|
-      (start_x..end_x).each do |col|
-        return false if @grid[row].nil? || @grid[col].nil?
-        return false if @grid.flatten.include?(" #{crate.product_code}") || @grid[row][col] != " ."
+  def crate_overlaps_another_crate?(crate, positions)
+    (positions[:start_y]..positions[:end_y]).each do |row|
+      (positions[:start_x]..positions[:end_x]).each do |col|
+        return false if @grid[row].nil? || @grid[col].nil? ||
+        @grid.flatten.include?(" #{crate.product_code}") ||
+        @grid[row][col] != " ."
       end
     end
+    true
+  end
 
-    if crate.width > @width || crate.height > @height
-      false
-    elsif start_x > @width || end_x > @width
-      false
-    elsif start_y > @height || end_y > @height
-      false
-    else
-      true
+  def crate_fit_within_boundaries?(crate, positions)
+    return false if crate.width > @width || crate.height > @height ||
+    positions[:start_x] > @width || positions[:end_x] > @width ||
+    positions[:start_y] > @height || positions[:end_y] > @height
+    true
+  end
+
+  def crate_can_be_placed?(crate, positions)
+    crate_overlaps_another_crate?(crate, positions) &&
+    crate_fit_within_boundaries?(crate, positions)
+  end
+
+  def place_crate(crate, positions)
+    (positions[:start_y]..positions[:end_y]).each do |row|
+      (positions[:start_x]..positions[:end_x]).each do |col|
+        @grid[row][col] = " #{crate.product_code}"
+      end
     end
   end
 end
